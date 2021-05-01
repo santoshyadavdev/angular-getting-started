ae9d17bb76b264282dec3557b0d706c735f8a0c5
Santosh Yadav <santosh.yadav198613@gmail.com>
2021-05-01 22:16:54 +0530

    data binding, component introduction

--- Angular-Demo/ecomApp/src/app/app.component.ts
+++ Angular-Demo/ecomApp/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'ecom-root',
   templateUrl: './app.component.html',
+  // template: `<h1>Hello World</h1>
+  //   <div>from Template</div>`,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'ecomApp';
+
+
+  role= 'Admin';
+
+
 }
--- Angular-Demo/ecomApp/src/app/app.module.ts
+++ Angular-Demo/ecomApp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
